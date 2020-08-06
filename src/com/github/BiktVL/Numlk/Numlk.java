4ce0319111b14539e696ea0e3e9b1e3812822ee3
Vladimir <31984806+BiktVL@users.noreply.github.com>
2020-08-06 21:46:10 +0500

    Add keyListener

--- src/com/github/BiktVL/Numlk/Numlk.java
+++ src/com/github/BiktVL/Numlk/Numlk.java
@@ -1,151 +1,236 @@
 package com.github.BiktVL.Numlk;
 
+import static java.awt.Label.RIGHT;
 import java.awt.Button;
 import java.awt.Frame;
 import java.awt.Label;
-import static java.awt.Label.RIGHT;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
-public class Numlk extends Frame{
-	Numlk(){
-		addWindowListener(new WindowAdapter(){
-			public void windowClosing(WindowEvent e){
-				dispose();
-			}
-		});
-		Button btn0=new Button("0");
-		Button btn1=new Button("1");
-		Button btn2=new Button("2");
-		Button btn3=new Button("3");
-		Button btn4=new Button("4");
-		Button btn5=new Button("5");
-		Button btn6=new Button("6");
-		Button btn7=new Button("7");
-		Button btn8=new Button("8");
-		Button btn9=new Button("9");
-		Button btnPoint=new Button(".");
-		Button btnPlus=new Button("+");
-		Button btnMinus=new Button("-");
-		Button btnMultiply=new Button("*");
-		Button btnDivision=new Button("/");
-		Label lbl=new Label("0",RIGHT);
-		btn0.setBounds(10,215,105,50);
-		btn1.setBounds(10,160,50,50);
-		btn2.setBounds(65,160,50,50);
-		btn3.setBounds(120,160,50,50);
-		btn4.setBounds(10,105,50,50);
-		btn5.setBounds(65,105,50,50);
-		btn6.setBounds(120,105,50,50);
-		btn7.setBounds(10,50,50,50);
-		btn8.setBounds(65,50,50,50);
-		btn9.setBounds(120,50,50,50);
-		btnPoint.setBounds(120,215,50,50);
-		btnPlus.setBounds(175,215,50,50);
-		btnMinus.setBounds(175,160,50,50);
-		btnMultiply.setBounds(175,105,50,50);
-		btnDivision.setBounds(175,50,50,50);
+// NumLockTrainer
+public class Numlk {
+	private Frame mainFrame;
+	private Label lbl = new Label();	
+	private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnPoint, btnPlus, btnMinus, btnMultiply, btnDivision, btnEqual;
+	
+	private void prepareGUI() {
+		
 		lbl.setBounds(10,15,215,50);
-		btn0.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("0");
+		lbl.setFocusable(true);
+		lbl.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {
+				lbl.setText(KeyEvent.getKeyText(e.getKeyCode()));
+			}
+			public void keyReleased(KeyEvent e) {
+			}
+			public void keyTyped(KeyEvent e) {
 			}
 		});
-		btn1.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("1");
-			}
-		});
-		btn2.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("2");
-			}
-		});
-		btn3.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("3");
-			}
-		});
-		btn4.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("4");
-			}
-		});
-		btn5.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("5");
-			}
-		});
-		btn6.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("6");
-			}
-		});
+		
+		btn7 = new Button();
+		btn7.setBounds(10,70,50,50);
+		btn7.setFocusable(false);
 		btn7.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				lbl.setText("7");
 			}
 		});
+		
+		btn8 = new Button();
+		btn8.setBounds(65,70,50,50);
+		btn8.setFocusable(false);
 		btn8.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				lbl.setText("8");
 			}
 		});
+		
+		btn9 = new Button();
+		btn9.setBounds(120,70,50,50);
+		btn9.setFocusable(false);
 		btn9.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				lbl.setText("9");
 			}
 		});
-		btnPoint.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText(".");
-			}
-		});
-		btnPlus.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("+");
-			}
-		});
-		btnMinus.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("-");
-			}
-		});
-		btnMultiply.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				lbl.setText("*");
-			}
-		});
+		
+		btnDivision = new Button();
+		btnDivision.setBounds(175,70,50,50);
+		btnDivision.setFocusable(false);
 		btnDivision.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				lbl.setText("/");
 			}
 		});
-		add(btn0);
-		add(btn1);
-		add(btn2);
-		add(btn3);
-		add(btn4);
-		add(btn5);
-		add(btn6);
-		add(btn7);
-		add(btn8);
-		add(btn9);
-		add(btnPoint);
-		add(btnPlus);
-		add(btnMinus);
-		add(btnMultiply);
-		add(btnDivision);
-		add(lbl);
-		setSize(240,280);
-		setLayout(null);
-		setVisible(true);
-	}  
+		
+		btn4 = new Button();
+		btn4.setBounds(10,125,50,50);
+		btn4.setFocusable(false);
+		btn4.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("4");
+			}
+		});
+
+		btn5 = new Button();
+		btn5.setBounds(65,125,50,50);
+		btn5.setFocusable(false);
+		btn5.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("5");
+			}
+		});
+		
+		btn6 = new Button();
+		btn6.setBounds(120,125,50,50);
+		btn6.setFocusable(false);
+		btn6.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("6");
+			}
+		});
+		
+		btnMultiply = new Button();
+		btnMultiply.setBounds(175,125,50,50);
+		btnMultiply.setFocusable(false);
+		btnMultiply.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("*");
+			}
+		});
+		
+		btn1 = new Button();
+		btn1.setBounds(10,180,50,50);
+		btn1.setFocusable(false);
+		btn1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("1");
+			}
+		});
+		
+		btn2 = new Button();
+		btn2.setBounds(65,180,50,50);
+		btn2.setFocusable(false);
+		btn2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("2");
+			}
+		});
+		
+		btn3 = new Button();
+		btn3.setBounds(120,180,50,50);
+		btn3.setFocusable(false);
+		btn3.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("3");
+			}
+		});
+		
+		btnMinus = new Button();
+		btnMinus.setBounds(175,180,50,50);
+		btnMinus.setFocusable(false);
+		btnMinus.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("-");
+			}
+		});		
+		
+		btn0 = new Button();
+		btn0.setBounds(10,235,50,50);
+		btn0.setFocusable(false);
+		btn0.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("0");
+			}
+		});
+		
+		btnEqual = new Button();
+		btnEqual.setBounds(65,235,50,50);
+		btnEqual.setFocusable(false);
+		btnEqual.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("=");
+			}
+		});
+				
+		btnPoint = new Button();
+		btnPoint.setBounds(120,235,50,50);
+		btnPoint.setFocusable(false);
+		btnPoint.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText(".");
+			}
+		});
+		
+		btnPlus = new Button();
+		btnPlus.setBounds(175,235,50,50);
+		btnPlus.setFocusable(false);
+		btnPlus.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				lbl.setText("+");
+			}
+		});
+			
+		mainFrame = new Frame();
+		mainFrame.setSize(235,295);
+		mainFrame.setLayout(null);
+		mainFrame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+		mainFrame.add(lbl);
+		mainFrame.add(btn0);
+		mainFrame.add(btn1);
+		mainFrame.add(btn2);
+		mainFrame.add(btn3);
+		mainFrame.add(btn4);
+		mainFrame.add(btn5);
+		mainFrame.add(btn6);
+		mainFrame.add(btn7);
+		mainFrame.add(btn8);
+		mainFrame.add(btn9);
+		mainFrame.add(btnEqual);
+		mainFrame.add(btnPoint);
+		mainFrame.add(btnPlus);
+		mainFrame.add(btnMinus);
+		mainFrame.add(btnMultiply);
+		mainFrame.add(btnDivision);
+		mainFrame.setVisible(true);
+	}
 	
-	public static void main(String args[]){
-	Numlk f=new Numlk();  
+	private void showNumlk() {
+		mainFrame.setTitle("Numlock keyboard");
+		lbl.setText("0");
+		lbl.setAlignment(RIGHT);
+		btn0.setLabel("0");
+		btn1.setLabel("1");
+		btn2.setLabel("2");
+		btn3.setLabel("3");
+		btn4.setLabel("4");
+		btn5.setLabel("5");
+		btn6.setLabel("6");
+		btn7.setLabel("7");
+		btn8.setLabel("8");
+		btn9.setLabel("9");
+		btnEqual.setLabel("=");
+		btnPoint.setLabel(".");
+		btnPlus.setLabel("+");
+		btnMinus.setLabel("-");
+		btnMultiply.setLabel("*");
+		btnDivision.setLabel("/");
+	}
+	
+	public Numlk() {
+		prepareGUI();
+	}
+	
+	public static void main(String[] args) {
+		Numlk numlk = new Numlk();
+		numlk.showNumlk();
 	}
 }
